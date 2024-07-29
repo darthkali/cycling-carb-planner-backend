a74f76117c44915352bb21ea06bf1c8fdc1c114b
dannysteinbrecher <danny.steinbrecher@outlook.com>
2024-07-29 13:02:50 +0200

    add strava api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.openapi.generator") version "7.7.0"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
 }
@@ -24,6 +25,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.webjars:chartjs:2.9.3")
 
+    // Open Api 3 Client
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.moshi:moshi-adapters:1.15.1")
+
+
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
@@ -32,6 +39,12 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+
+// ----------------------------------------------------------------------------
+//  OpenApi Client
+apply("src/main/resources/apis/strava/strava-openapi.gradle.kts")
+
 
 kotlin {
     compilerOptions {
